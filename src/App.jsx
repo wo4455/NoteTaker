3c839ce01a92031b1d3251fd97cbe0c91f731ee9
Will Otwell <90997466+wo4455@users.noreply.github.com>
2023-08-09 16:40:39 -0500

    Create pin feature and fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,66 @@
-import './App.css'
 import React from 'react';
 import Form from './components/Form';
 import Note from './components/Note';
+import './App.css'
+
+const colors = ['bg-red-400', 'bg-green-400', 'bg-blue-400', 'bg-yellow-400', 'bg-orange-400'];
 
 const App = () => {
-  const [ notesList, setNotesList ] = React.useState([])
+  const [ notesList, setNotesList ] = React.useState([]);
 
-  const handleClear = () => {
-    setNotesList([])
-  }
+  const handlePin = (item) => { // set pinned to true/false
+    const notesListClone = [ ...notesList ];
+    const index = notesListClone.indexOf(item);
+    notesListClone[index].isPinned = !notesListClone[index].isPinned;
+    setNotesList(notesListClone);
+  };
+
+  const getColor = () => {
+    const index = notesList.length % colors.length;
+    return colors[index];
+  };
 
   return (
     <>
-      <Form onSubmitForm={inputValue => setNotesList([...notesList, { data: inputValue }])}/> {/* get form data from props */}
+      <Form
+        onSubmitForm={
+          (inputValue) => {
+            setNotesList([
+              ...notesList,
+              {
+                data: inputValue,
+                order: notesList.length,
+                bgColor: getColor(),
+                isPinned: false,
+              },
+            ]);
+          }
+        }
+      />
       
       <div className='w-full flex justify-between'>
         <p className='w-1/5 mb-2 mt-20 text-gray-400 text-sm'>PINNED</p>
-        <button className='w-1/5 mb-2 mt-20 text-gray-400 text-sm hover:opacity-80' onClick={handleClear}>Clear All</button>
+        <button className='w-1/5 mb-2 mt-20 text-gray-400 text-sm hover:opacity-80' onClick={() => setNotesList([])}>Clear All</button>
       </div>
       
       <div className='w-full flex flex-wrap justify-center items-start'>
         {
-          notesList.map((item, index) => (
-            <Note data={item.data} key={item + index}/>
-          ))
+          notesList
+            .sort((a, b) => {
+              if (a.isPinned && b.isPinned) return 0;
+              if (a.isPinned) return -1;
+              if (b.isPinned) return 1;
+              return a.order > b.order ? 1 : -1;
+            })
+            .map((item, index) => (
+              <Note
+                note={item} 
+                onPinned={() => handlePin(item)}
+                key={item + index}
+              />
+            ))
         }
-      </div>
-      
+      </div>    
     </>
   )
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,32 +2,18 @@ import React from 'react';
 import { AiFillPushpin } from 'react-icons/ai';
 import { AiOutlinePushpin } from 'react-icons/ai';
 
-const colors = ['bg-red-400', 'bg-green-400', 'bg-blue-400', 'bg-yellow-400', 'bg-orange-400'];
-
-const Note = ({ data, onSetPinned }) => {
-    const [ isPinned, setPinned ] = React.useState(false);
-    const [ bg, setBg ] = React.useState('');
-
-    const getColor = () => {
-        const randNum = Math.floor(Math.random() * 5);
-        return colors[randNum];
-    }
-
-    React.useEffect(() => {
-        setBg(getColor())
-    }, [])
-
+const Note = ({ note, onPinned }) => {
     return (
-        <div className={`w-1/4 ${bg} drop-shadow-md m-5 rounded-md`}>
+        <div className={`w-1/4 ${note.bgColor} drop-shadow-md m-5 rounded-md`}>
             <div className='w-full p-3'>
-                {isPinned 
+                {note.isPinned
                 ? 
-                    <AiFillPushpin fontSize={25} className='cursor-pointer' onClick={() => setPinned(false)}/>
+                    <AiFillPushpin fontSize={25} className='cursor-pointer' onClick={onPinned}/>
                 : 
-                    <AiOutlinePushpin fontSize={25} className='cursor-pointer opacity-50 hover:opacity-100' onClick={() => setPinned(true)}/>
+                    <AiOutlinePushpin fontSize={25} className='cursor-pointer opacity-50 hover:opacity-100' onClick={onPinned}/>
                 }
                 <p className='p-5 leading-6 break-words mb-3'>
-                    {data}
+                    {note.data}
                 </p>
             </div>
         </div>
